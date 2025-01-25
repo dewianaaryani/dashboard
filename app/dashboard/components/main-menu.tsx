3429d32983507400250e357c5dce5daa96e0bcf4
Iqbal Ardiansyah <aiqbalsyah@gmail.com>
2025-01-26 00:55:07 +0700

    feat: add dashboard content

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -7,24 +7,24 @@ import { LightDarkToggle } from '@/components/ui/light-dark-toggle'
 
 export default function MainMenu() {
   return (
-    <div className='bg-muted overflow-auto p-4 flex flex-col'>
-        <div className='border-b dark:border-b-black border-b-zinc-300 pb-4'>
+    <nav className='bg-muted overflow-auto p-4 flex flex-col'>
+        <header className='border-b dark:border-b-black border-b-zinc-300 pb-4'>
             <MenuTitle />
-        </div>
-        <div className='py-4 grow'>
+        </header>
+        <ul className='py-4 grow'>
             <MenuItem href='/dashboard'>My Dashboard</MenuItem>
             <MenuItem href='/dashboard/teams'>Teams</MenuItem>
             <MenuItem href='/dashboard/employees'>Employees</MenuItem>
-            <MenuItem href='/dashboard/accounts'>Account</MenuItem>
+            <MenuItem href='/dashboard/account'>Account</MenuItem>
             <MenuItem href='/dashboard/settings'>Settings</MenuItem>  
-        </div>
-        <div className='flex gap-2 items-center'>
+        </ul>
+        <footer className='flex gap-2 items-center'>
             <Avatar>
                 <AvatarFallback className='bg-pink-300 dark:bg-pink-500'>U</AvatarFallback>
             </Avatar>
             <Link href="/" className='hover:underline'>Logout</Link>
             <LightDarkToggle className='ml-auto' />
-        </div>
-    </div>
+        </footer>
+    </nav>
   )
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import React from 'react'
+import EmployeesStats from './components/employees/employees-stats'
+import TeamsStats from './components/teams/teams-stats'
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <Tabs defaultValue='employees'>
+      <TabsList className='mb-4'>
+        <TabsTrigger value='employees'>
+          Employees stats
+        </TabsTrigger>
+        <TabsTrigger value='teams'>
+          Teams stats
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value='employees'>
+        <EmployeesStats />
+      </TabsContent>
+      <TabsContent value='teams'>
+        <TeamsStats />
+      </TabsContent>
+    </Tabs>
   )
 }
